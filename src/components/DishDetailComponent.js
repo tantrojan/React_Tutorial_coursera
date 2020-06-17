69e0e87392f66edd1fff4f7be13dc67a4ae23ada
tantrojan <tg.99ghosh@gmail.com>
2020-06-17 18:50:50 +0530

    Combining Reducers

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   Card,
   CardImg,
@@ -6,33 +6,157 @@ import {
   CardBody,
   CardText,
   Breadcrumb,
-  BreadcrumbItem
+  BreadcrumbItem,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Row,
+  Col,
+  Input,
+  Label,
 } from "reactstrap";
+import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
 
-const RenderComments = ({ comments }) => {
-  const com = comments.map(comment => {
-    const date = new Intl.DateTimeFormat("en-US", {
-      year: "numeric",
-      month: "short",
-      day: "2-digit"
-    }).format(new Date(Date.parse(comment.date)));
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+
+class CommentForm extends Component {
+  handleSubmit(values) {
+    console.log("Current State is: " + JSON.stringify(values));
+    alert("Current State is: " + JSON.stringify(values));
+  }
+
+  render() {
     return (
-      <li key={comment.id}>
-        <p>{comment.comment}</p>
-        <p>
-          -- {comment.author} , {date}
-        </p>
-      </li>
+      <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+        <Row className="form-group">
+          <Label htmlFor="rating" md={12}>
+            Rating
+          </Label>
+          <Col md={12}>
+            <Control.select
+              model=".rating"
+              name="rating"
+              className="form-control"
+            >
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Control.select>
+          </Col>
+        </Row>
+        <Row className="form-group">
+          <Label htmlFor="author" md={12}>
+            Your Name
+          </Label>
+          <Col md={12}>
+            <Control.text
+              model=".author"
+              id="author"
+              name="author"
+              placeholder="Your Name"
+              className="form-control"
+              validators={{
+                required,
+                minLength: minLength(3),
+                maxLength: maxLength(15),
+              }}
+            />
+            <Errors
+              className="text-danger"
+              model=".author"
+              show="touched"
+              messages={{
+                required: "Required",
+                minLength: "Must be greater than 2 characters",
+                maxLength: "Must be 15 characters or less",
+              }}
+            />
+          </Col>
+        </Row>
+        <Row className="form-group">
+          <Label htmlFor="comment" md={12}>
+            Comment
+          </Label>
+          <Col md={12}>
+            <Control.textarea
+              model=".comment"
+              id="comment"
+              name="comment"
+              className="form-control"
+            />
+          </Col>
+        </Row>
+        <Row className="form-group">
+          <Col md={12}>
+            <Button type="submit" color="primary">
+              Submit
+            </Button>
+          </Col>
+        </Row>
+      </LocalForm>
     );
-  });
-  return (
-    <div className="col-12 col-md-5 m-1">
-      <h4>Comments</h4>
-      <ul class="list-unstyled">{com}</ul>
-    </div>
-  );
-};
+  }
+}
+
+class RenderComments extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isModalOpen: false,
+    };
+
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+
+  render() {
+    const { comments } = this.props;
+    const com = comments.map((comment) => {
+      const date = new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "2-digit",
+      }).format(new Date(Date.parse(comment.date)));
+      return (
+        <li key={comment.id}>
+          <p>{comment.comment}</p>
+          <p>
+            -- {comment.author} , {date}
+          </p>
+        </li>
+      );
+    });
+    return (
+      <>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+          <ModalBody>
+            <CommentForm />
+          </ModalBody>
+        </Modal>
+        <div className="col-12 col-md-5 m-1">
+          <h4>Comments</h4>
+          <ul class="list-unstyled">{com}</ul>
+          <Button outline onClick={this.toggleModal}>
+            <span className="fa fa-pencil fa-lg"></span> Submit Comment
+          </Button>
+        </div>
+      </>
+    );
+  }
+}
 
 const RenderDish = ({ dish }) => {
   return (
@@ -48,7 +172,7 @@ const RenderDish = ({ dish }) => {
   );
 };
 
-const DishDetail = props => {
+const DishDetail = (props) => {
   return (
     <div className="container">
       <div className="row">
